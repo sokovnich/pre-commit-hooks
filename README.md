f986c3275f783ec297fc5b1af3bdbd10a931e892
Yan Sokovnich <x6@live.ru>
2026-01-29 14:37:50 +0300

    add support for issue ID regex and message template customization

--- README.md
+++ README.md
@@ -3,10 +3,17 @@ python pre-commit hooks
 
 ## inject-issue-name
 
-This hook inserts issue name part from branch name into end of commit-message
+Hook to insert issue identity part from branch name into commit-message  
+if commit message does not contain issue identity already. 
+
+Install:
+```
+pip install pre-commit
+pre-commit install --hook-type prepare-commit-msg
+```
 
 Example:
 
 branch name ==> `feature/61235-enable-authentication`  
 original commit message ==> `authentication enabled by default`  
-modified commit message ==> `authentication enabled by default #61235`    
+modified commit message ==> `authentication enabled by default #61235`  
--- pre_commit_hooks/inject_issue_name.py
+++ pre_commit_hooks/inject_issue_name.py
@@ -1,27 +1,51 @@
 import argparse
+import logging
 import re
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
+logger = logging.getLogger()
 
-branch_name_regex = r'(feature|hotfix|bugfix|chore)/(\d+)-.+'
-
+branch_name_regex = r'(?:feature|hotfix|bugfix|chore)/(\d+)-.+'
+msg_template = "{msg} {issue_id}"
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help='Commit message file path')
+    parser.add_argument(
+        '--branch-name-regex',
+        default=branch_name_regex,
+        help=f'Regex to extract issue ID from branch name'
+    )
+    parser.add_argument(
+        '--msg-template',
+        default=msg_template,
+        help=f'Message template'
+    )
     args = parser.parse_args(argv)
 
     commit_msg_file_path = args.filename
-    branch_name = check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip().decode()
 
-    matched = re.match(branch_name_regex, branch_name)
+    try:
+        branch_name = check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip().decode()
+    except CalledProcessError:
+        logger.warning("Cannot get current HEAD, maybe rebase is in progress?")
+        return 0
+
+    matched = re.match(args.branch_name_regex, branch_name)
 
     if matched:
-        issue_id = matched.group(2)
+        issue_id = matched.group('issue_id')
+        if issue_id.isdigit():
+            issue_id = f"#{issue_id}"
+
         with open(commit_msg_file_path, 'r+') as fh:
             commit_msg = fh.read()
+            if issue_id in commit_msg:
+                logger.warning("Issue ID already found in commit message")
+                return 0
+
             fh.seek(0, 0)
-            fh.write('%s #%s' % (commit_msg, issue_id))
+            fh.write(args.msg_template.format(msg=commit_msg, issue_id=issue_id))
 
     return 0
 
